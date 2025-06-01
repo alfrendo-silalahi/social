96506f2fdc0d1ba626731907a95e55ce851168f1
alfrendo-silalahi <alfrendos72@gmail.com>
2025-06-01 20:09:33 +0700

    implement repository pattern & create persisting data with sql

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,14 +4,16 @@ import (
 	"log"
 
 	"github.com/alfrendo-silalahi/social/internal/env"
+	"github.com/alfrendo-silalahi/social/internal/store"
 )
 
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
 	}
+	store := store.NewStorage(nil)
 
-	app := &application{config: cfg}
+	app := &application{config: cfg, store: store}
 	mux := app.mount()
 
 	log.Fatal(app.run(mux))
